8a7737790de4bbc8c9f2d466164469466106706e
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-25 12:33:04 +0900

    deploy.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.fog_directory  = 'freemarketapp63'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketapp63'
   end
 end
