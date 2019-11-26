223c305cdb4b07958352b23ff66a4b0889e46352
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-26 12:32:08 +0900

    devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
-
+  
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -17,7 +17,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:image).permit(:name, :image, :price, :ship_way, :ship_price, :ship_date, :condition)
+    params.require(:item).permit(:name, :image, :price, :ship_way, :ship_price, :ship_date, :condition)
   end
-
 end
