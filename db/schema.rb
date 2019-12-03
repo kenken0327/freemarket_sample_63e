54afe5b497e25f0862a139ebe79d0834571abf17
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-12-03 14:17:46 +0900

    index.html.haml update

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,16 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this  definition.
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this .rb definition is the authoritative source for your
-# database . If you need to create the application database on another
-# system, you should be using db::load, not running all the migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::.define(version: 2019_11_28_054403) do
+ActiveRecord::Schema.define(version: 2019_11_28_054403) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -69,6 +69,7 @@ ActiveRecord::.define(version: 2019_11_28_054403) do
     t.string "tell_no", null: false
     t.text "self_info"
     t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
