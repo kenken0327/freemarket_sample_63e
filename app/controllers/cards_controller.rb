f51b3411f6a8c28db19e8c32e2d39c46e8d6a64b
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-04 14:41:18 +0900

    delete commentout

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,14 +4,11 @@ class CardsController < ApplicationController
 
   def index
     card = Card.where(user_id: current_user.id).first
-    #テーブルからpayjpの顧客IDを検索
     if card.blank?
       redirect_to new_card_path
     else
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-      #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
-      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
   end
@@ -20,9 +17,9 @@ class CardsController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-      :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
-      :customer => card.customer_id, #顧客ID
-      :currency => 'jpy', #日本円
+      :amount => 13500
+      :customer => card.customer_id
+      :currency => 'jpy'
     )
   
     redirect_to done_cards_path
@@ -31,7 +28,7 @@ class CardsController < ApplicationController
   def done
   end
 
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
+  def new
     gon.new_key = ENV['PAYJP_PUBLIC_KEY']
     card = Card.where(user_id: current_user.id).first
     if card.present?
@@ -39,19 +36,19 @@ class CardsController < ApplicationController
     end
   end
 
- # indexアクションはここでは省略
+ 
 
-  def create #PayjpとCardのデータベースを作成
+  def create
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     if params['payjp-token'].blank?
       redirect_to items_path
     else
-      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
+     
       customer = Payjp::Customer.create(
-        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
+        description: 'test',
         email: current_user.email,
-        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
-        metadata: {user_id: current_user.id} # 無くてもOK。
+        card: params['payjp-token'], 
+        metadata: {user_id: current_user.id}
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
