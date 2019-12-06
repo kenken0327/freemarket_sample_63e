db3e0ef86bd4e67b72c6d888ed47066cc1d6cc50
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-06 15:16:17 +0900

    add FB login

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,18 @@ class User < ApplicationRecord
   validates :month_id, presence: true
   validates :date_id, presence: true
   validates :tell_no, presence: true, format: {with: /\A\d{3}[-]\d{4}[-]\d{4}\z/}
+  
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+  end
+end
 
 end
