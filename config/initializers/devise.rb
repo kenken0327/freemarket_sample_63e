b0010586146e6f26e008edd928854ca6542b6031
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-06 15:45:50 +0900

    delete commenout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,8 +301,8 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.omniauth :facebook, FACEBOOK_ID=ENV["FACEBOOK_ID"], FACEBOOK_SECRET_KEY=ENV["FACEBOOK_SECRET_KEY"]
   config.omniauth :google_oauth2,
-                  ENV['GOOGLE_APP_ID'], # 環境変数に先ほど控えたクライアントIDを入れておく
-                  ENV['GOOGLE_APP_SECRET'], # 環境変数に先ほど控えたシークレットを入れておく
+                  ENV['GOOGLE_APP_ID']
+                  ENV['GOOGLE_APP_SECRET']
                   name: :google,
                   scope: %w(email) 
 end
