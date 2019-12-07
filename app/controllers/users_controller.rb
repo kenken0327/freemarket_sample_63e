fe7c0cce45038eedc74d75ddb73bc60e805c5d14
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-07 18:14:58 +0900

    add before_action to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_user, only: [:index,:show,:edit,:update]
+
   def index
-    @user = User.find(current_user.id)
     @items = Item.where(saler: current_user.id)
   end
 
   def show
-    @user = User.find(current_user.id)
   end
 
   def edit
-    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(current_user.id)
     @user.update(user_params)
     redirect_to action: :show
   end
@@ -23,6 +22,10 @@ private
 
   def user_params
     params.require(:user).permit(:nickname,:self_info)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 
 end
