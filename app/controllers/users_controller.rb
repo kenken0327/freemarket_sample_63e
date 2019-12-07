1635940cabab53a381f1885b60b5df59f835ed8d
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-07 17:09:52 +0900

    add user-edit,update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to action: :show
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:nickname,:self_info)
+  end
+
 end
