02bb02757d38a642dfdd115783fdbfe12e1db4d7
OnumaAyumi <onuayu.min@gmail.com>
2019-12-10 15:15:01 +0900

    card-view,header

--- db/schema.rb
+++ db/schema.rb
@@ -44,16 +44,14 @@ ActiveRecord::Schema.define(version: 2019_12_06_051321) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.string "name", null: false
-    t.text "image", null: false
-    t.integer "price", null: false
-    t.string "ship_way", null: false
-    t.string "ship_price", null: false
-    t.string "ship_date", null: false
-    t.string "condition", null: false
-    t.integer "buyer"
+    t.text "image"
+    t.integer "price"
+    t.string "ship_way"
+    t.string "ship_price"
+    t.string "ship_date"
+    t.string "condition"
     t.integer "saler", null: false
-    t.text "description", null: false
-    t.string "ship_place", null: false
+    t.integer "buyer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
@@ -81,8 +79,6 @@ ActiveRecord::Schema.define(version: 2019_12_06_051321) do
     t.string "tell_no", null: false
     t.text "self_info"
     t.string "image"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
