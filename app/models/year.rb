f90c36567d6b6e30e6ea173e294afad12dc7671c
shuheiyasuda <yasu.canes07@gmail.com>
2019-12-10 14:32:44 +0900

    users/edit 、year.rb 修正

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,5 +1,19 @@
 class Year < ActiveHash::Base
-  self.data = [  {id: 1, name:1990 }, {id: 2, name: 1991}, {id: 3, name: 1992},
-    {id: 4, name: 1993},{id: 5, name: 1994},{id: 6, name: 1995}]
-
+  self.data = [ {id:1, name: 1970}, {id:2, name: 1971}, {id:3, name: 1972},
+                {id:4, name: 1973}, {id:5, name: 1974}, {id:6, name: 1975},
+                {id:7, name: 1976}, {id:8, name: 1977}, {id:9, name: 1978},
+                {id:10,name: 1979}, {id:11,name: 1980}, {id:12,name: 1981},
+                {id:13,name: 1982}, {id:14,name: 1983}, {id:15,name: 1984},
+                {id:16,name: 1985}, {id:17,name: 1986}, {id:18,name: 1987},
+                {id:19,name: 1988}, {id:20,name: 1989}, {id:21,name: 1990},
+                {id:22,name: 1991}, {id:23,name: 1992}, {id:24,name: 1993},
+                {id:25,name: 1994}, {id:26,name: 1995}, {id:27, name:1996},
+                {id:28,name: 1997}, {id:29, name:1998}, {id:30, name:1999},
+                {id:31,name: 2000}, {id:32,name: 2001}, {id:33,name: 2002},
+                {id:34,name: 2003}, {id:35,name: 2004}, {id:36,name: 2005},
+                {id:37,name: 2006}, {id:38,name: 2007}, {id:39, name:2008},
+                {id:40, name:2009}, {id:41,name: 2010}, {id:42,name: 2011},
+                {id:43,name: 2012}, {id:44,name: 2013}, {id:45,name: 2014},
+                {id:46,name: 2015}, {id:47,name: 2016}, {id:48,name: 2017}
+              ]
   end
